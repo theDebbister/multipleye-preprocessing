7d967b6aea13d2e04eaae3f9cfd5ee2d837aed61
Andreas Säuberli <andreas.saeuberli@gmail.com>
2024-11-24 22:01:46 +0100

    Fix image paths, check if files exist

--- quality-report/stimulus.py
+++ quality-report/stimulus.py
@@ -37,43 +37,47 @@ class Stimulus:
 
     @classmethod
     def load(
-        cls, stim_dir: Path, lang: str, country: str, labnum: int, stimulus_name: str
+        cls, stimulus_dir: Path, lang: str, country: str, labnum: int, stimulus_name: str
     ):
-        stimulus_df = pd.read_excel(
-            stim_dir / f"multipleye_stimuli_experiment_{lang}.xlsx"
-        )
+        assert stimulus_name in NAMES, f"{stimulus_name!r} is not a valid stimulus name"
+        stimulus_df_path = stimulus_dir / f"multipleye_stimuli_experiment_{lang}.xlsx"
+        stimulus_df = pd.read_excel(stimulus_df_path)
         stimulus_rows = stimulus_df[stimulus_df["stimulus_name"] == stimulus_name]
-        assert len(stimulus_rows) == 1
+        assert len(stimulus_rows) == 1, f"{len(stimulus_rows)} rows found for stimulus {stimulus_name} in {stimulus_df_path}"
+
         stimulus_row = stimulus_rows.iloc[0]
         stimulus_id = int(stimulus_row["stimulus_id"])
         stimulus_type = stimulus_row["stimulus_type"]
+
         pages = []
         for column, value in stimulus_row.items():
             if column.startswith("page_"):
                 page_number = int(column.split("_")[1])
                 if pd.notna(value):
+                    image_filename = f"{stimulus_name.lower()}_id{stimulus_id}_page_{page_number}_{lang}.png"
+                    image_path=stimulus_dir / f"stimuli_images_{lang}_{country}_{labnum}" / image_filename
                     page = StimulusPage(
                         number=page_number,
                         text=value,
-                        image_path=stim_dir
-                        / f"stimuli_images_{lang}_{country}_{labnum}",
+                        image_path=image_path,
                     )
+                    assert page.image_path.exists(), f"File {page.image_path} does not exist"
                     pages.append(page)
-        return cls(
+
+        stimulus = cls(
             id=stimulus_id,
             name=stimulus_name,
             type=stimulus_type,
             pages=pages,
         )
+        return stimulus
 
 
 if __name__ == "__main__":
-    stim_dir = Path("010_ZH_CH_1_ET1/stimuli_MultiplEYE_zh_ch_Zurich_1_2025")
-
+    stimulus_dir = Path("010_ZH_CH_1_ET1/stimuli_MultiplEYE_zh_ch_Zurich_1_2025")
     lang = "zh"
     country = "ch"
     labnum = 1
     stimulus_name = "PopSci_MultiplEYE"
 
-    stimulus = Stimulus.load(stim_dir, lang, country, labnum, stimulus_name)
-    print(stimulus)
+    stimulus = Stimulus.load(stimulus_dir, lang, country, labnum, stimulus_name)
